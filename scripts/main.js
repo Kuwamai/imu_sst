2ef2c50c2e6530522a7142214d506557adbefb21
Kuwamai <mitsumex333@gmail.com>
2018-01-11 16:13:32 +0000

    anomaly scoreのグラフ表示がでけた

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,39 @@ ros.on('connection', function() {console.log('websocket: connected');});
 ros.on('error', function(error) {console.log('websocket error: ', error); });
 ros.on('close', function() {console.log('websocket: closed');});
 
+var data = [{
+	y: [],
+	type: 'lines',
+	line: {color: '#80CAF6'}
+}];
+
+var options = {
+	  title: 'Anomaly score'
+};
+
+Plotly.plot('graph', data, options);
+
 var ls = new ROSLIB.Topic({
 	ros : ros,
 	name : '/anomaly_score',
 	messageType : 'std_msgs/Float64'
 });
 
+var array = [];
+
 ls.subscribe(function(message) {
-	str = JSON.stringify(message);
+	str = JSON.stringify(message.data);
 	document.getElementById("imu_sst").innerHTML = str;
-	console.log(str);                                  
+	console.log(str);
+
+	array.push(message.data);
 });
+
+var interval = setInterval(function() {
+	Plotly.extendTraces('graph', {
+	y: [array]
+	}, [0])
+	
+	array = [];
+
+}, 100);
